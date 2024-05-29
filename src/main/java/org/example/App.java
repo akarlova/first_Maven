c80250c0d6d5deeec1ee5d673f5812332e5d844c
aka.rlova <ladyerial88@gmail.com>
2024-05-29 22:40:42 +0300

    calculator added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,40 @@
 package org.example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "It is a simple calculator" );
-    }
+public class App {
+    public static void main(String[] args) {
+
+if(args.length != 3) {
+    System.out.println("oh no, there more, that three arguments");
 }
+
+            double num1 = Double.parseDouble(args[0]);
+            String operation = args[1];
+            double num2 = Double.parseDouble(args[2]);
+
+
+                double result = 0;
+                switch (operation) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "x":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 == 0) {
+                            System.out.println("You can't divide by zero");
+                        }
+                        result = num1 / num2;
+                        break;
+                    default:
+                        System.out.println("Invalid operation");
+                        break;
+                }
+                System.out.println("Result: " + result);
+        }
+
+    }
+
