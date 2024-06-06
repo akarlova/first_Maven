41084dd792e98bbed0a0dcd0fe1367bff5a48a5f
aka.rlova <ladyerial88@gmail.com>
2024-06-06 20:53:41 +0300

    changes according to the notes added

--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -7,37 +7,33 @@ import org.junit.Test;
 public class SolutionTest {
 
     @Test
-    public void jewelsShouldBeInStones() {
-
-        Assert.assertEquals(6, Solution.numJewelsInStones("Aaa",
-                "AAaaqzz"));
-
+    public void numJewelsInStones_JewelsAreInStones_Success() {
+        Assert.assertEquals(6, Solution.numJewelsInStones("Aaa", "AAaaqzz"));
     }
 
     @Test
-    public void jewelsAreNotInStones() {
-        Assert.assertEquals(0, Solution.numJewelsInStones("z",
-                "ZZZ"));
+    public void numJewelsInStones_JewelsAreNotInStones() {
+        Assert.assertEquals(0, Solution.numJewelsInStones("z", "ZZZ"));
     }
 
     @Test
-    public void arrayHasGoodPairs() {
+    public void numIdenticalPairs_ArrayHasGoodPairs() {
         Assert.assertEquals(4, Solution.numIdenticalPairs(new int[]{1, 2, 3, 1, 1, 3}));
     }
     @Test
-    public void arrayHasOnlyGoodPairs() {
+    public void numIdenticalPairs_ArrayOnlyHasGoodPairs() {
         Assert.assertEquals(6, Solution.numIdenticalPairs(new int[]{1, 1, 1, 1}));
     }
     @Test
-    public void noGoodPairsInArray() {
+    public void numIdenticalPairs_ArrayHasNoGoodPairs() {
         Assert.assertEquals(0, Solution.numIdenticalPairs(new int[]{1,2,3}));
     }
     @Test
-    public void textShouldHaveBalloon(){
+    public void maxNumberOfBalloons_TextConsistsWordBalloon(){
         Assert.assertEquals(2, Solution.maxNumberOfBalloons("ballutloonballllootryn"));
     }
     @Test
-    public void textShouldHaveNoBalloon(){
+    public void maxNumberOfBalloons_TextDoesntConsistWordBalloon(){
         Assert.assertEquals(0, Solution.maxNumberOfBalloons("baloon"));
     }
 }
